d47e03e03c8718df48ee0292cbe75ed2b3491527
Souradip Manna <souradipyomanna@gmail.com>
2025-04-09 17:28:37 +0530

    9th fresh

---
+++ server/server.js
@@ -0,0 +1,64 @@
+// import express from 'express'
+// import cors from 'cors'
+// import 'dotenv/config'
+// import connectDB from './configs/mongodb.js'
+// import { clerkWebhooks } from './controllers/webhooks.js'
+
+// // Initialize Express
+// const app = express()
+
+// // Middlewares
+// app.use(cors())
+// app.use(express.json())
+
+// // Routes
+// app.get('/', (req, res) => res.send("API Working"))
+// app.post('/clerk', clerkWebhooks)
+
+// // Port
+// const PORT = process.env.PORT || 5000
+
+// // Wrap in async function to allow top-level await
+// const startServer = async () => {
+//   try {
+//     await connectDB()
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`)
+//     })
+//   } catch (error) {
+//     console.error("Error connecting to DB:", error)
+//   }
+// }
+
+// startServer()
+
+
+// export default app
+
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js';
+
+
+// Initialize Express
+const app = express()
+
+// Connect to database
+await connectDB()
+
+// Middlewares
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhooks)
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
