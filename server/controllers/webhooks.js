3cd80b85927c29e26a301bb1189a2117ce20c7d8
Souradip Manna <souradipyomanna@gmail.com>
2025-04-09 19:11:31 +0530

    last correction

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -16,7 +16,7 @@ export const clerkWebhooks = async (req, res) => {
         const {data, type} = req.body
         
         switch (type) {
-            case 'User.created' : {
+            case 'user.created' : {
                 const userData = {
                     _id : data.id,
                     email : data.email_addresses[0].email_address,
