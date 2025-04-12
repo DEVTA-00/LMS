29db3d22329319855c950c6180f5c20e1d38f04f
Souradip Manna <souradipyomanna@gmail.com>
2025-04-13 01:51:30 +0530

    13.04.2025

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -6,13 +6,13 @@ export const protectEducator = async (req, res, next) => {
         const userId = req.auth.userId
         const response = await clerkClient.users.getUser(userId)
 
-        if(response.publicMetadata.role !== educator) {
+        if(response.publicMetadata.role !== 'educator') {
             return res.json({ success: false, message: 'Unauthorized Access'})
         }
 
         next()
 
     } catch (error) {
-        res.json({ success: false, message: error.message})
+        res.json({ success: false, message: error.message })
     }
 }
--- server/server.js
+++ server/server.js
@@ -43,6 +43,8 @@ import { clerkWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 
 
@@ -61,6 +63,8 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter)
 
 // Port
 const PORT = process.env.PORT || 5000
